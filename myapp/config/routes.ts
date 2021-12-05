e222cc648322a143ba809abd1c2b272254e2f429
KingCross9-34 <2568930409@qq.com>
2021-12-05 16:22:30 +0800

    individual profile

---
+++ myapp/config/routes.ts
@@ -0,0 +1,70 @@
+export default [
+  {
+    path: '/user',
+    layout: false,
+    routes: [
+      {
+        path: '/user',
+        routes: [
+          {
+            name: 'login',
+            path: '/user/login',
+            component: './user/Login',
+          },
+        ],
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    path: '/welcome',
+    name: 'welcome',
+    icon: 'smile',
+    component: './Welcome',
+  },
+  {
+    path: '/admin',
+    name: 'admin',
+    icon: 'crown',
+    access: 'canAdmin',
+    component: './Admin',
+    routes: [
+      {
+        path: '/admin/sub-page',
+        name: 'sub-page',
+        icon: 'smile',
+        component: './Welcome',
+      },
+      {
+        component: './404',
+      },
+    ],
+  },
+  {
+    name: 'list.table-list',
+    icon: 'table',
+    path: '/list',
+    component: './TableList',
+  },
+  {
+    path: '/',
+    redirect: '/welcome',
+  },
+  {
+    name: '个人中心',
+    icon: 'smile',
+    path: '/accountcenter',
+    component: './AccountCenter',
+  },
+  {
+    name: '空白页面',
+    icon: 'smile',
+    path: '/emptypage',
+    component: './EmptyPage',
+  },
+  {
+    component: './404',
+  },
+];
